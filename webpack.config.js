63aacb090d2e01217c8f5d8ef4c28911838437b9
Andrew Turley <andrew.turley@achilles.com>
2017-05-04 14:52:02 +0100

    Webpack 2 update

--- webpack.config.js
+++ webpack.config.js
@@ -2,47 +2,56 @@ var webpack = require('webpack');
 var path = require('path');
 
 module.exports = {
-  entry: [
-    'script!jquery/dist/jquery.min.js',
-    'script!foundation-sites/dist/foundation.min.js',
-    './app/app.jsx'
-  ],
-  externals: {
-    jquery: 'jQuery'
-  },
-  plugins: [
-    new webpack.ProvidePlugin({
-      '$': 'jquery',
-      'jQuery': 'jquery'
-    })
-  ],
-  output: {
-    path: __dirname,
-    filename: './public/bundle.js'
-  },
-  resolve: {
-    root: __dirname,
-    alias: {
-      applicationStyles: 'app/styles/app.scss'
+    entry: [
+        'script-loader!jquery/dist/jquery.min.js',
+        'script-loader!foundation-sites/dist/js/foundation.min.js',
+        './app/app.jsx'
+    ],
+    externals: {
+        jquery: 'jQuery'
     },
-    extensions: ['', '.js', '.jsx']
-  },
-  module: {
-    loaders: [
-      {
-        loader: 'babel-loader',
-        query: {
-          presets: ['react', 'es2015', 'stage-0']
+    plugins: [
+        new webpack.ProvidePlugin({
+            '$': 'jquery',
+            'jQuery': 'jquery'
+        })
+    ],
+    output: {
+        path: __dirname,
+        filename: './public/bundle.js'
+    },
+    resolve: {
+        modules: [
+            path.resolve(__dirname, 'node_modules')
+        ],
+        alias: {
+            applicationStyles: path.resolve(__dirname, 'app/styles/app.scss')
         },
-        test: /\.jsx?$/,
-        exclude: /(node_modules|bower_components)/
-      }
-    ]
-  },
-  sassLoader: {
-    includePaths: [
-      path.resolve(__dirname, './node_modules/foundation-sites/scss')
-    ]
-  },
-  devtool: 'cheap-module-eval-source-map'
+        extensions: ['.js', '.jsx']
+    },
+    module: {
+        rules: [{
+                test: /\.jsx?$/,
+                exclude: /(node_modules|bower_components)/,
+                use: [{
+                    loader: 'babel-loader',
+                    query: {
+                        presets: ['react', 'es2015', 'stage-0']
+                    }
+                }]
+            },
+            {
+                test: /\.scss$/,
+                use: [{
+                    loader: 'sass-loader',
+                    options: {
+                        includePaths: [
+                            path.resolve(__dirname, './node_modules/foundation-sites/scss')
+                        ]
+                    }
+                }],
+            }
+        ]
+    },
+    devtool: 'cheap-module-source-map'
 };
